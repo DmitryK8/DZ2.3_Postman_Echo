dac11da9015b6fc819ced342966e652b191c9b67
Dmitry Kravchenko <d.k.12.1972@gmail.com>
2021-12-14 13:23:20 +0500

    первая заливка

---
+++ src/test/java/rest/MobileBankApiTestV1.java
@@ -0,0 +1,26 @@
+package rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .header("Content-Length", String.valueOf(424))
+                .body("data", equalTo("some data"));
+
+    }
+}
